a05a693ce2c8796e182acf0fce4c57efe1df731b
Ian Laan <ianlaan@outlook.com>
2018-09-01 12:06:25 +0200

    Add project files.

---
+++ Shino/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
